4f9c18c312420c7ee6faa2cfaddbf6b4a4f17f24
Ryan "archaen" Pando <vretha@gmail.com>
2017-05-23 10:36:06 -0600

    Rabbit factor and config

--- episodic-shows/src/main/java/com/example/AmqpListener.java
+++ episodic-shows/src/main/java/com/example/AmqpListener.java
@@ -1,11 +1,23 @@
 package com.example;
 
 @Configuration
-public class AmqpListener {
+public class AmqpListener implements RabbitListenerConfigurer {
 
     @RabbitListener(queues = "episodic-shows-queue")
     public void receiveMessage(final Message message) {
 
+    }
+
+    @Bean
+    public DefaultMessageHandlerMethodFactory messageHandlerMethodFactory() {
+        DefaultMessageHandlerMethodFactory factory = new DefaultMessageHandlerMethodFactory();
+        factory.setMessageConverter(new MappingJackson2MessageConverter());
+        return factory;
+    }
+
+    @Override
+    public void configureRabbitListeners(final RabbitListenerEndpointRegistrar registrar) {
+        registrar.setMessageHandlerMethodFactory(messageHandlerMethodFactory());
     }
 
 }
