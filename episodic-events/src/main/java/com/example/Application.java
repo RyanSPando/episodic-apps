ff59283cb2416f6a6eccbc2ddb5488b265e4d5f5
Ryan "archaen" Pando <vretha@gmail.com>
2017-05-24 14:51:34 -0600

    Gateway

--- episodic-events/src/main/java/com/example/Application.java
+++ episodic-events/src/main/java/com/example/Application.java
@@ -2,11 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class DemoApplication {
+@EnableDiscoveryClient
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
